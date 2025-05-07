7f84edf16d798399af2ac1055d3ae25ea6532ce6
alexkm174x8 <akongmontoya@hotmail.com>
2025-05-07 14:00:43 -0600

    feat: protected routes

---
+++ app/actions.ts
@@ -0,0 +1,41 @@
+import { createServerClient } from '@supabase/ssr'
+import { cookies } from 'next/headers'
+
+const supabaseUrl = 'https://dijctnuytoiqorvkcjmq.supabase.co'
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+
+export async function getServerSession() {
+  const cookieStore = cookies()
+  
+  const supabase = createServerClient(
+    supabaseUrl,
+    supabaseKey!,
+    {
+      cookies: {
+        get(name: string) {
+          return cookieStore.get(name)?.value
+        },
+      },
+    }
+  )
+
+  return supabase.auth.getSession()
+}
+
+export async function getServerUser() {
+  const cookieStore = cookies()
+  
+  const supabase = createServerClient(
+    supabaseUrl,
+    supabaseKey!,
+    {
+      cookies: {
+        get(name: string) {
+          return cookieStore.get(name)?.value
+        },
+      },
+    }
+  )
+
+  return supabase.auth.getUser()
+} 
