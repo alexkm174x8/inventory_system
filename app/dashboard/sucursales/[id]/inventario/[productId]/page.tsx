385c8b5264e4cda3ae69ae1d00dcf7c72f80caad
alexkm174x8 <akongmontoya@hotmail.com>
2025-05-15 17:08:36 -0600

    fix: more fixes for prod v8 plsss work

--- app/dashboard/sucursales/[id]/inventario/[productId]/page.tsx
+++ app/dashboard/sucursales/[id]/inventario/[productId]/page.tsx
@@ -1,12 +1,14 @@
 'use client';
 
-import React from 'react';
+import { useRouter } from 'next/navigation';
 import ProductDetailView from '@/components/ProductDetailView';
 
-export default function Page() {
+export default function ProductDetailPage() {
+  const router = useRouter();
+
   return (
     <div className='h-full m-5'>
-      <ProductDetailView  />
+      <ProductDetailView onClose={() => router.back()} />
     </div>
   );
 }
